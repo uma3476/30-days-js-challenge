07695f642f5e638c79a479f0b5f2ceec4a83d706
uma3476 <umanaidu347@gmail.com>
2022-08-26 10:56:46 +0530

    day_1 completed

---
+++ day-1.js
@@ -0,0 +1,24 @@
+// Declaring different variables of different data types
+
+let firstName = 'uma'    
+let lastName = 'jagata'      
+let country = 'India'       
+let city ='Amalapuram'        
+let age = 100                 
+
+
+// Declaring variables with number values
+
+const gravity = 9.81         
+const boilingPoint = 100     
+const PI = 3.14              
+
+// Variables can also be declaring in one line separated by comma
+
+let name = 'uma',     
+  job = 'software',
+  live = 'Amalapuram'
+
+  console.log(firstName,lastName,country,city,age)
+  console.log(gravity,boilingPoint,PI)
+  console.log(name,job,live)
